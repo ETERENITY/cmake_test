6545a75e7b877e3514ba2cb0a4489e9789eae094
y li <1138931449@qq.com>
2023-09-29 03:38:58 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <thread>
+#include "add.h"
+int main(){
+    std::cout << "hello world" << add(1, 2) << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+}
