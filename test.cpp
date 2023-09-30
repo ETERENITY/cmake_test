78cd3cfe97233733041bb2d70e5c068b427f8972
y li <1138931449@qq.com>
2023-10-01 03:15:28 +0800

    [fix] how to ues SHARED LIB

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include "add.h"
 #include "sub.h"
 int main(){
+    int i = 1;
     std::cout << "hello world" << add(1, 2) << sub(2, 1) << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
