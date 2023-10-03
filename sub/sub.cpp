bad85c8cafd66f6ee83101fd083ac32f2679d652
y li <1138931449@qq.com>
2023-10-04 01:26:09 +0800

    [add] martix

--- sub/sub.cpp
+++ sub/sub.cpp
@@ -1,4 +1,28 @@
-#include "sub.h"
+﻿#include "sub.h"
+#include <stdexcept>
 int sub(int x,int y){
 	return x - y;
+}
+
+martix::martix(int row, int cow) {
+	this->row = row;
+	this->cow = cow;
+	this->data = new int[cow * row]();
+	if (!this->data) {
+		throw std::runtime_error("malloc error");
+	}
+}
+
+martix::martix(int row, int cow, int* data) {
+	this->row = row;
+	this->cow = cow;
+	this->data = new int[cow * row]();
+	if (!this->data) {
+		throw std::runtime_error("malloc error");
+	}
+	memcpy(this->data, data, cow * row * sizeof(int));
+}
+
+martix::~martix(){
+	delete[] this->data;
 }
--- sub/sub.h
+++ sub/sub.h
@@ -1,2 +1,13 @@
-#pragma once
+﻿#pragma once
+#include <memory>
 __declspec(dllexport) int sub(int x, int y);
+
+class __declspec(dllexport) martix {
+	martix(const int row, const int cow);
+	martix(int row, int cow, int* data);
+	~martix();
+private:
+	int row = 0;
+	int cow = 0;
+	int* data = nullptr;
+};
