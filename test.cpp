5a9ae297fc417b8091747d30c54e2d5895357af6
y li <1138931449@qq.com>
2023-10-04 01:26:48 +0800

    [add]

--- test.cpp
+++ test.cpp
@@ -5,6 +5,8 @@
 int main(){
     int i = 1;
     std::cout << "hello world" << add(1, 2) << sub(2, 1) << std::endl;
+    int data[4]{ 1,2,3,4 };
+    martix mat(2, 2, data);
     std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
 }
